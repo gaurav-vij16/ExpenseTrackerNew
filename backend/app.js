751c75c6ed3c81a90a7126c7802a23a4c9211b58
gaurav-vij16 <gaurav.21scse1010546@galgotiasuniversity.edu.in>
2025-04-02 03:37:13 +0530

    database url changed

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,9 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
+
+console.log('DB_URL:', process.env.DB_URL);
+
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/', middlewareAuth, protectedRoutes);
 
@@ -25,10 +28,10 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
 });
 
-
 // Database connection
-mongoose.connect(process.env.DB_URL)
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
+        console.log("Database connected successfully");
         app.listen(PORT, () => {
             console.log(`Server listening on port ${PORT}`);
         });
